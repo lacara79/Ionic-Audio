a7642059608d89fed2f9cfc9ddd2c7e343ef46f8
Luiz Alexandre da Costa Araujo <lacara79@gmail.com>
2023-11-14 18:26:29 -0300

    first commint

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { VoiceRecorder, VoiceRecorderPlugin, RecordingData, GenericResponse, CurrentRecordingStatus } from 'capacitor-voice-recorder';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+  audio: any;
+  async som() {
 
+    const url = await Filesystem.getUri({
+      directory: Directory.Documents,
+      path: '../../assets/beth.mp3',
+    })
+
+    this.audio = new Audio(url.uri);
+    this.audio.play();
+  }
+
+  parar() {
+    this.audio.pause();
+  }
+
+  async startRecording() {
+    try {
+      const result = await VoiceRecorder.startRecording();
+      console.log('Recording started', result);
+    } catch (error) {
+      console.error('Error starting recording', error);
+    }
+  }
+
+  async stopRecording() {
+    try {
+      const result = await VoiceRecorder.stopRecording();
+      console.log('Recording stopped', result);
+    } catch (error) {
+      console.error('Error stopping recording', error);
+    }
+  }
+  
 }
